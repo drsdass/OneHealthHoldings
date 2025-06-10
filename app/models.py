3d285cadf91e041b7d15a3606f273cfe3d11fcdb
drsdass <satishsdass@gmail.com>
2025-06-10 13:48:33 -0700

    Add files via upload

---
+++ app/models.py
@@ -0,0 +1,21 @@
+
+from app import db
+from flask_login import UserMixin
+from app import login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    role = db.Column(db.String(50), nullable=False, default='user')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
