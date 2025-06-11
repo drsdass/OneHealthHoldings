288996963f4834b06180a1886c91486f97b72c20
drsdass <satishsdass@gmail.com>
2025-06-11 08:51:39 -0700

    Add files via upload

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,10 +1,35 @@
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 from flask_login import login_required, current_user
 
-main_bp = Blueprint('main', __name__)
+main = Blueprint('main', __name__)
 
-@main_bp.route('/dashboard')
+permissions = {
+    'SatishD': ['First Bio Lab', 'First Bio Genetics', 'First Bio Lab of Illinois', 'AIM Laboratories', 'AMICO Dx', 'Enviro Labs', 'Stat Labs'],
+    'JayM': ['First Bio Lab', 'First Bio Genetics', 'First Bio Lab of Illinois', 'AIM Laboratories', 'AMICO Dx', 'Enviro Labs', 'Stat Labs'],
+    'AndrewS': ['First Bio Genetics', 'AIM Laboratories'],
+    'AshlieT': ['First Bio Lab', 'First Bio Genetics', 'First Bio Lab of Illinois', 'AIM Laboratories', 'AMICO Dx', 'Enviro Labs', 'Stat Labs'],
+    'VinceO': ['AMICO Dx'],
+    'ACG': ['First Bio Lab', 'First Bio Genetics', 'First Bio Lab of Illinois', 'AIM Laboratories', 'AMICO Dx', 'Enviro Labs', 'Stat Labs'],
+    'SonnyN': ['AMICO Dx'],
+    'MinaK': ['First Bio Lab', 'First Bio Genetics', 'First Bio Lab of Illinois', 'AIM Laboratories', 'AMICO Dx', 'Enviro Labs', 'Stat Labs'],
+    'NickC': ['AMICO Dx'],
+    'MelindaC': ['First Bio Lab', 'First Bio Genetics', 'First Bio Lab of Illinois', 'AIM Laboratories', 'AMICO Dx', 'Enviro Labs', 'Stat Labs'],
+    'Omar': ['First Bio Lab', 'First Bio Genetics', 'First Bio Lab of Illinois', 'AIM Laboratories', 'AMICO Dx', 'Enviro Labs', 'Stat Labs'],
+    'DarangT': ['AMICO Dx', 'Enviro Labs'],
+    'BobS': ['First Bio Lab', 'First Bio Genetics', 'First Bio Lab of Illinois', 'AIM Laboratories', 'AMICO Dx', 'Enviro Labs', 'Stat Labs'],
+    'BobSilverang': ['First Bio Lab', 'First Bio Genetics', 'First Bio Lab of Illinois', 'Stat Labs'],
+    'AghaA': ['AMICO Dx'],
+    'Wenjun': ['AMICO Dx'],
+    'NickT': ['First Bio Lab', 'First Bio Genetics', 'First Bio Lab of Illinois', 'AIM Laboratories', 'AMICO Dx', 'Enviro Labs', 'Stat Labs'],
+    'AndreaM': ['AMICO Dx'],
+    'BenM': ['Stat Labs'],
+    'WeesL': ['Stat Labs'],
+}
+
+@main.route('/dashboard')
 @login_required
 def dashboard():
-    return render_template('main/dashboard.html', user=current_user)
+    username = current_user.username
+    allowed_entities = permissions.get(username, [])
+    return render_template('dashboard.html', entities=allowed_entities)
